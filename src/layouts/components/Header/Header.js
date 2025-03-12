975e8bcc3f9ee554da804f3a087b12842b9027b2
vinhtuongluong <vinhtuongluong@gmail.com>
2025-03-12 16:22:00 +0700

    Refactor project structure, simplify component imports and add PropTypes

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -21,7 +21,8 @@ import Menu from '~/components/Popper/Menu';
 import { UploadIcon, MessageIcon, InboxIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 import Search from '../Search';
-import routesConfig from '~/config/routes';
+import config from '~/config';
+
 const cx = classNames.bind(styles);
 
 const MENU_ITEMS = [
@@ -172,11 +173,11 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
 
-                <Search></Search>
+                <Search />
 
                 <div className={cx('actions')}>
                     {currentUser ? (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,1 @@
-function Home() {
-    return <h2 style={{ height: '2000px' }}>Home page</h2>;
-}
-
-export default Home;
+export { default } from './Home';
